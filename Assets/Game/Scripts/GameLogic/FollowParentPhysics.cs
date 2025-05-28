e893b72ca1bf69876f3febb293724b7938a7a055
Velsimir <nintendocoin@yandex.ru>
2025-05-28 18:12:37 +0300

    Follow Parent added

---
+++ Assets/Game/Scripts/GameLogic/FollowParentPhysics.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class FollowParentPhysics : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private float _followForce = 500f;
+
+    private void FixedUpdate()
+    {
+        if (_rigidbody == null || transform.parent == null)
+            return;
+
+        Vector3 targetPosition = transform.parent.position;
+        Vector3 direction = targetPosition - transform.position;
+
+        _rigidbody.linearVelocity = Vector3.zero;
+        _rigidbody.AddForce(direction * _followForce * Time.fixedDeltaTime, ForceMode.VelocityChange);
+    }
+}
